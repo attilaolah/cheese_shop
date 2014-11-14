9ced94d15eff830d4bf6d24fbd645474170a7fc7
Attila Oláh <attilaolah@gmail.com>
2014-11-14 11:00:28 +0100

    add a test case

--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ShopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a shop with cheeses" do
+    shop = Shop.create!(
+      name: "The Whistling Cheese",
+      cheeses: [
+        Cheese.create!(name: "Bree"),
+        Cheese.create!(name: "Ementaler"),
+        Cheese.create!(name: "Kačkavalj"),
+      ],
+    )
+    assert_equal 3, shop.cheeses.count
+  end
 end
